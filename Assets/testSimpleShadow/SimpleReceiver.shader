e954f4234475a91d3d419d24d15973b3f912e9e5
DreamChaseAndVera <42426249+DreamChaseAndVera@users.noreply.github.com>
2021-12-28 16:02:14 +0800

    CSM 和 简单的阴影
    CSM 和 简单的阴影

---
+++ Assets/testSimpleShadow/SimpleReceiver.shader
@@ -0,0 +1,71 @@
+﻿Shader "Simple/SimpleReceiver"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 pos:TEXCOORD1;
+				float4 vertex : SV_POSITION;
+			};
+			
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			sampler2D _depthTexture;
+			//变换到灯光摄像机的 vp矩阵 从外部传入
+			float4x4 _vpMatrix;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				//vp矩阵和m矩阵相乘 组合出光源空间下的mvp
+				_vpMatrix = mul(_vpMatrix, unity_ObjectToWorld);
+				o.pos = mul(_vpMatrix,v.vertex);
+
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				//对深度图在屏幕空间下进行采样
+				fixed4 dcol = tex2Dproj(_depthTexture,i.pos);
+				//把取得的值进行解码，获取深度图时的EncodeFloatRGBA
+				float d = DecodeFloatRGBA(dcol);
+				float d1 = i.vertex.z / i.vertex.w;
+				if (d1 > d)
+				{
+					col *= 0.5;
+				}
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/testSimpleShadow/TestSimpleShadow.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class TestSimpleShadow:MonoBehaviour
+{
+    Camera m_renderCam;
+    RenderTexture rt;
+
+    GameObject plane;
+    Material material;
+
+    Matrix4x4 st = new Matrix4x4();
+    private void Awake()
+    {
+        st.SetRow(0,new Vector4(0.5f, 0,   0,   0.5f));
+        st.SetRow(1,new Vector4(0,    0.5f,0,   0.5f));
+        st.SetRow(2,new Vector4(0,    0,   0.5f,0.5f));
+        st.SetRow(3,new Vector4(0f,   0,   0,   1f));
+        
+        GameObject camera = new GameObject("Camera2");
+        m_renderCam = camera.AddComponent<Camera>();
+        m_renderCam.cullingMask = 1 << LayerMask.NameToLayer("UI");
+        m_renderCam.orthographic = false;
+        m_renderCam.backgroundColor = Color.white;
+        camera.transform.parent = transform;
+        camera.transform.localPosition = Vector3.zero;
+        camera.transform.localScale = Vector3.one;
+        camera.transform.localRotation = Quaternion.Euler(0,0,0);
+        
+        rt = new RenderTexture(512,512,24);
+        m_renderCam.targetTexture = rt;
+        m_renderCam.SetReplacementShader(Shader.Find("Simple/SimpleCamera"), "RenderType");
+
+        plane = GameObject.Find("Plane");
+        material = plane.GetComponent<MeshRenderer>().material;
+        material.SetTexture("_depthTexture",rt);
+
+        
+        Matrix4x4 vp = GL.GetGPUProjectionMatrix(m_renderCam.projectionMatrix, false) * m_renderCam.worldToCameraMatrix;
+        vp = st * vp;
+        material.SetMatrix("_vpMatrix", vp);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
